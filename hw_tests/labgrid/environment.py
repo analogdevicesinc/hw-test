90edb494c3bffceab0732ccb05bc314056182291
Ozan Durgut <ozan.durgut@analog.com>
2026-06-18 11:29:31 +0200

    apply labgrid defaults from context
    Signed-off-by: Ozan Durgut <ozan.durgut@analog.com>

--- hw_tests/labgrid/environment.py
+++ hw_tests/labgrid/environment.py
@@ -1,4 +1,3 @@
-import os
 from contextlib import contextmanager
 
 from labgrid import Environment
@@ -6,25 +5,9 @@ from labgrid import Environment
 from hw_tests.labgrid.hosts import install_host_aliases
 
 
-def env_path_from_pytest(pytestconfig):
-    return (
-        pytestconfig.getoption("lg_env", default=None)
-        or os.environ.get("LABGRID_ENV")
-        or os.environ.get("LG_ENV")
-    )
-
-
-def coordinator_from_pytest(pytestconfig):
-    return (
-        pytestconfig.getoption("lg_coordinator", default=None)
-        or os.environ.get("LG_COORDINATOR")
-        or os.environ.get("LABGRID_COORDINATOR")
-    )
-
-
 @contextmanager
-def labgrid_environment(config_file, coordinator=None):
-    install_host_aliases()
+def labgrid_environment(config_file, coordinator=None, ssh_hosts=None):
+    install_host_aliases(ssh_hosts)
     env = Environment(config_file)
     if coordinator:
         env.config.set_option("coordinator_address", coordinator)
@@ -34,9 +17,3 @@ def labgrid_environment(config_file, coordinator=None):
     finally:
         env.cleanup()
 
-
-def get_target(env, name, config_file):
-    target = env.get_target(name)
-    if target is None:
-        raise AssertionError(f"target {name!r} not found in {config_file}")
-    return target
