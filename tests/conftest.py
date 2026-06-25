f5952d07ddc52515b9b826ee3e5e9258f8026426
Jorge Marques <jorge.marques@analog.com>
2026-06-25 15:37:33 +0200

    tests/conftest: remove pytest_addoption
    Options are passed through the context or environment.
    The classes will kindly fail in a similar manner, depending
    on what the test needs, e.g.,:
    
        if require_place is True:
      >     assert self.coordinator, "neither labgrid_coordinator in context ..."
                   ^^^^^^^^^^^^^^^^
      E     AssertionError: neither labgrid_coordinator in context or LG_COOR...
    
    hw_tests/labgrid/labgrid_client.py:34: AssertionError
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,20 +48,3 @@ def _addoption_once(group, *args, **kwargs):
         if "already added" not in str(exc):
             raise
 
-
-def pytest_addoption(parser):
-    labgrid = parser.getgroup("labgrid")
-    _addoption_once(
-        labgrid,
-        "--lg-env",
-        action="store",
-        default=None,
-        help="Path to the Labgrid environment configuration file.",
-    )
-    _addoption_once(
-        labgrid,
-        "--lg-coordinator",
-        action="store",
-        default=None,
-        help="Labgrid coordinator address. Falls back to LG_COORDINATOR or LABGRID_COORDINATOR.",
-    )
